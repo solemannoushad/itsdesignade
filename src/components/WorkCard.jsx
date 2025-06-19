b3b22324ca05b045da1c66d281f3355b0637e567
solemannoushad <solemannoushad31@gmail.com>
2025-06-19 03:13:05 -0700

    video-editing content added in work card

--- src/components/WorkCard.jsx
+++ src/components/WorkCard.jsx
@@ -21,22 +21,20 @@ function WorkCard() {
     <div className='flex flex-col w-[70%] h-screen'>
       <div className="work-card-wrapper border-l-1 border-l-slate-200 w-full h-full flex flex-col">
         <div className="work-card-img w-full h-[70%]">
-            <Image src={'/images/Image00084.png'} className='w-full h-full object-cover object-top' width={1400} height={1400} alt='Nood Mood' />
+            <Image src={'/images/video-editing.png'} className='w-full h-full object-cover object-top' width={1400} height={1400} alt='Nood Mood' />
         </div>
         <div className="work-card-tags flex w-full p-5 itens-center justify-between">
             <div className="card-tags flex items-center gap-5">
-                <CardTags title={"SEO"} />
-                <CardTags title={"Meta Ads"} />
-                <CardTags title={"Google Ads"} />
+                <CardTags title={"Short Reels"} />
+                <CardTags title={"Animation"} />
             </div>
             <div className="card-category flex items-center gap-7">
-                <CardCategory title={"ecommerce"} />
-                <CardCategory title={"shopify"} />
-                <CardCategory title={"branding"} />
+                <CardCategory title={"Premier Pro"} />
+                <CardCategory title={"After Effects"} />
             </div>
         </div>
         <div className="work-card-title flex items-center justify-center py-10">
-            <p className='text-6xl font-bold'>Digital Marketing</p>
+            <p className='text-6xl font-bold'>Video Editing</p>
         </div>
       </div>
     </div>
