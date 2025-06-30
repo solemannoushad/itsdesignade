445bcb7d7658dc2f6dbf4c5087a0ce4010d30c38
solemannoushad <solemannoushad31@gmail.com>
2025-06-30 11:27:37 -0700

    images added in services steps

--- src/components/ServicesSteps.jsx
+++ src/components/ServicesSteps.jsx
@@ -1,25 +1,25 @@
+import { servicesSteps } from "@/content/services";
+import Image from "next/image";
 import React from "react";
 
-const services = [
-  "Ecommerce",
-  "Social Media",
-  "Video Editing",
-  "Web Development",
-  "Digital Marketing",
-  "Graphic Designing",
-];
 
 function ServiceSteps() {
   return (
     <section className="w-full h-screen bg-white text-black pl-36 py-10">
-      {services.map((service, index) => {
+      {servicesSteps.map((service, index) => {
+        const {title , img}= service
         return (
-          <div className='service-title flex items-center gap-4 my-5 cursor-default group'  style={{ paddingLeft: `${index * 120}px` }} key={index}>
+          <div className='service-title flex items-center gap-14 my-5 cursor-default relative group'  style={{ paddingLeft: `${index * 60}px` }} key={title}>
+
+            <div className="hover-img-wrapper w-[160px] h-[200px] overflow-hidden rounded-xl -rotate-10 absolute right-0">
+              <Image src={`/images/steps/${img}`} width={300} height={300} alt={service} />
+            </div>
+
             <div className="service-title-count bg-white text-black border-1 border-black w-10 h-10 flex items-center justify-center rounded-full text-xl group-hover:bg-black group-hover:text-white duration-300">
               {index + 1}
             </div>
-            <div className="service-title-title text-[5rem] font-semibold">
-              {service}
+            <div className="service-title-title text-[5rem] font-semibold group-hover:scale-90 duration-300">
+              {title}
             </div>
           </div>
         );
--- src/content/services.js
+++ src/content/services.js
@@ -1,46 +1,54 @@
 export const servicesData = [
-    {
-        title: "Video Editing",
-        image: "video-editing.jpg",
-        tags: ["SEO", "Meta Ads", "Google Ads"],
-        tools: ['Premiere Pro', 'After Effects'],
-        objectFit: "top"
-    },
-    {
-        title: "Digital Marketing",
-        image: "digital-marketing.jpg",
-        tags: ["Short Reels", "Animation"],
-        tools: ['ecommerce', 'shopify', 'branding'],
-        objectFit: "top"
-    },
-    
-    {
-        title: "Graphic Designing",
-        image: "graphic-designing.jpg",
-        tags: ["Posts" , "Logo", "Thumbnail" , "UI/UX"],
-        tools: ['Photoshop', 'Canva', 'Figma'],
-        objectFit: "center"
-    },
-    {
-        title: "Social Media Management",
-        image: "social-media.jpg",
-        tags: ["Account Management", "Content Creation"],
-        tools: ['Facebook', 'Instagram', 'TikTok'],
-        objectFit: "center"
-    },
-    {
-        title: "Web Development",
-        image: "web-development.jpg",
-        tags: ["Web Design", "Custom Websites"],
-        tools: ['WordPress', 'React', 'Next.js'],
-        objectFit: "top"
-    },
-    {
-        title: "E-commerce",
-        image: "ecommerce.jpg",
-        tags: ["Shopify", "WooCommerce"],
-        tools: ['Shopify', 'WooCommerce'],
-        objectFit: "top"
-    },
+  {
+    title: "Video Editing",
+    image: "video-editing.jpg",
+    tags: ["SEO", "Meta Ads", "Google Ads"],
+    tools: ["Premiere Pro", "After Effects"],
+    objectFit: "top",
+  },
+  {
+    title: "Digital Marketing",
+    image: "digital-marketing.jpg",
+    tags: ["Short Reels", "Animation"],
+    tools: ["ecommerce", "shopify", "branding"],
+    objectFit: "top",
+  },
 
-]
+  {
+    title: "Graphic Designing",
+    image: "graphic-designing.jpg",
+    tags: ["Posts", "Logo", "Thumbnail", "UI/UX"],
+    tools: ["Photoshop", "Canva", "Figma"],
+    objectFit: "center",
+  },
+  {
+    title: "Social Media Management",
+    image: "social-media.jpg",
+    tags: ["Account Management", "Content Creation"],
+    tools: ["Facebook", "Instagram", "TikTok"],
+    objectFit: "center",
+  },
+  {
+    title: "Web Development",
+    image: "web-development.jpg",
+    tags: ["Web Design", "Custom Websites"],
+    tools: ["WordPress", "React", "Next.js"],
+    objectFit: "top",
+  },
+  {
+    title: "E-commerce",
+    image: "ecommerce.jpg",
+    tags: ["Shopify", "WooCommerce"],
+    tools: ["Shopify", "WooCommerce"],
+    objectFit: "top",
+  },
+];
+
+export const servicesSteps = [
+  { title: "Ecommerce", img: "ecommerce.png" },
+  { title: "Social Media", img: "social-media.png" },
+  { title: "Video Editing", img: "video-editing.png" },
+  { title: "Digital Marketing", img: "digital-marketing.png" },
+  { title: "Graphic Designing", img: "graphic-designing.png" },
+  { title: "Web Development", img: "web-dev.png" },
+];
