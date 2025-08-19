36bd74722ac6862b1f0cd0b4ead669104e674c4d
solemannoushad <solemannoushad31@gmail.com>
2025-08-19 04:54:50 -0700

    portfolio hero text animation added

--- src/components/PortfolioHero.jsx
+++ src/components/PortfolioHero.jsx
@@ -26,23 +26,34 @@ function PortfolioHero() {
         ease: "elastic.out(1, 0.2)"
       });
     });
+
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: 1.4 });
+    for (let i = 0; i < 4; i++) {
+      const position = i === 0 ? ">" : "+=1";
+      tl.to(".animated-spans h1", {
+        yPercent: "-=100",
+        duration: 0.5,
+        ease: "none"
+      }, position);
+    }
+
   }, []);
 
   return (
     <div className="bg-black pt-64 pb-20">
-      <div className="text-[11vw] flex gap-0">
-        <h1 className="font-medium ml-20">
+      <div className="text-[10vw] flex gap-0">
+        <h1 className="font-medium ml-28">
           Do Epic
         </h1>
-        <div className="animated-spans font-bold text-primary relative bg-red-500 w-1/2 overflow-y-hidden">
-          <h1 className="absolute"><span>&nbsp; design</span></h1>
+        <div className="animated-spans font-bold text-primary relative overflow-y-hidden w-1/2 ">
+          <h1 className="absolute" style={{ transform: "translateY(0%)" }}><span>&nbsp; design</span></h1>
           <h1 className="absolute" style={{ transform: "translateY(100%)" }}><span>&nbsp; dev</span></h1>
           <h1 className="absolute" style={{ transform: "translateY(200%)" }}><span>&nbsp; creative</span></h1>
           <h1 className="absolute" style={{ transform: "translateY(300%)" }}><span>&nbsp; social</span></h1>
           <h1 className="absolute" style={{ transform: "translateY(400%)" }}><span>&nbsp; design</span></h1>
         </div>
       </div>
-      <div className="w-4/5 mx-auto">
+      <div className="w-[90%] mx-auto">
         <svg
           viewBox="0 0 900 200"
           className="w-full h-auto"
